0c76837805f64b2ddbcd914392d45dac36f9050f
Nicola Papale <nicola.papale@azuni.org>
2019-05-02 14:10:56 +0200

    Handle gracefully lockIds in manual testing
    Add a way to save and use the lockId returned and used by the various
    API endpoints
    
    Silence the curl download status
    
    Add a TODO to the documentation

--- documentation/manual-testing.sh
+++ documentation/manual-testing.sh
@@ -4,53 +4,59 @@
 
 # Get a list of available stories
 function stories {
-    curl http://localhost:8081/stories
+    curl -s http://localhost:8081/stories
 }
 
 # Get the content of given story
 # $1: story
 function story {
-    curl "http://localhost:8081/stories/$1"
+    curl -s "http://localhost:8081/stories/$1"
 }
 
 # Get the exclusive editing rights to a story
 # $1: story to aquire
 # $2: user name
+# $3: (optional) name of variable in which to store the lock id
 # prints request result (lockId if successfull)
 function open {
-    curl -H 'Content-Type: application/json' \
+    __lockId=$(curl -s -H 'Content-Type: application/json' \
         -d "{\"user\":\"$2\"}" \
-        "http://localhost:8081/stories/$1/open"
+        "http://localhost:8081/stories/$1/open")
+    echo $__lockId
+    declare -g ${3:-__lockId__}=$(sed 's/"\([^"]*\)"/\1/' <<<"$__lockId")
 }
 
 # Give up exclusive editing rights to a story
 # $1: story to give up exclusive rights to
-# $2: lockid
+# $2: (optional) the lock Id
 # prints request result
 function close {
-    curl -H 'Content-Type: application/json' \
-        -d "{\"lock\":\"$2\"}" \
+    __lockId=${2:-$__lockId__}
+    curl -s -H 'Content-Type: application/json' \
+        -d "{\"lock\":\"$__lockId\"}" \
         "http://localhost:8081/stories/$1/close"
 }
 
 # Renew story session rights
 # $1: story
-# $2: lockid
+# $2: (optional) the lock Id
 # prints request result
 function keepup {
-    curl -H 'Content-Type: application/json' \
-        -d "{\"lock\":\"$2\"}" \
+    __lockId=${2:-$__lockId__}
+    curl -s -H 'Content-Type: application/json' \
+        -d "{\"lock\":\"$__lockId\"}" \
         "http://localhost:8081/stories/$1/keepup"
 }
 
 # Save a story you own the editing rights of
 # $1: story
-# $2: lockid
-# $3: content
+# $2: content
+# $3: (optional) the lock Id
 # prints request result
 function save {
-    curl -H 'Content-Type: application/json' \
-        -d "{\"lock\":\"$2\", \"file\": \"$3\"}" \
+    __lockId=${3:-$__lockId__}
+    curl -s -H 'Content-Type: application/json' \
+        -d "{\"lock\":\"$__lockId\", \"file\": \"$2\"}" \
         "http://localhost:8081/stories/$1/save"
 }
 
